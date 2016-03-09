8a3290c80f7eca24579d4c7e423a700c31fdc0b2
dianaartiom <diana.artiom24@gmail.com>
2016-03-09 21:36:24 +0200

    Deleted conflicting migrations

--- patient-monitoring/db/migrate/20160214202735_add_fields_to_user.rb
+++
@@ -1,8 +0,0 @@
-class AddFieldsToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :type, :string
-    add_column :users, :birthday, :datetime
-    add_column :users, :height, :string
-    add_column :users, :weight, :string
-  end
-end
--- patient-monitoring/db/migrate/20160215164503_add_last_seen_to_cases.rb
+++
@@ -1,5 +0,0 @@
-class AddLastSeenToCases < ActiveRecord::Migration
-  def change
-    add_column :cases, :last_seen_at, :datetime
-  end
-end
